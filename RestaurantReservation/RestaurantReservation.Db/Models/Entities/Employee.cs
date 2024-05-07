40e398e00e82ce69c27ecba6e753a2de30487423
Yousef Abuali <abualiyousef@outlook.com>
2024-05-07 04:15:24 +0300

    Add 'EmployeePosition' enum

--- RestaurantReservation/RestaurantReservation.Db/Models/Entities/Employee.cs
+++ RestaurantReservation/RestaurantReservation.Db/Models/Entities/Employee.cs
@@ -1,3 +1,5 @@
+using RestaurantReservation.Db.Models.Enums;
+
 namespace RestaurantReservation.Db.Models.Entities;
 
 public class Employee
@@ -5,6 +7,7 @@ public class Employee
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public EmployeePosition Position { get; set; }
     public int? RestaurantId { get; set; }
     public Restaurant Restaurant { get; set; }
     public List<Order> Orders { get; set; } = new List<Order>();
---
+++ RestaurantReservation/RestaurantReservation.Db/Models/Enums/EmployeePosition.cs
@@ -0,0 +1,9 @@
+namespace RestaurantReservation.Db.Models.Enums;
+
+public enum EmployeePosition
+{
+    Waiter=1,
+    Chef=2,
+    Cashier=3,
+    Manager=4
+}
