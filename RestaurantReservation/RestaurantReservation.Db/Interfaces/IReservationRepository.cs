98de04289899f11fc63c7a178f21ecf4d0b7299c
Yousef Abuali <abualiyousef@outlook.com>
2024-05-08 23:52:44 +0300

    Add 'GetReservationsDetailsAsync' method

--- RestaurantReservation/RestaurantReservation.Db/Interfaces/IReservationRepository.cs
+++ RestaurantReservation/RestaurantReservation.Db/Interfaces/IReservationRepository.cs
@@ -1,7 +1,10 @@
 using RestaurantReservation.Db.Models.Entities;
+using RestaurantReservation.Db.Models.Views;
 
 namespace RestaurantReservation.Db.Interfaces;
 
 public interface IReservationRepository : IRepository<Reservation>
 {
+    Task<IEnumerable<Reservation>> GetReservationsByCustomerAsync(int customerId);
+    Task<IEnumerable<ReservationDetails>> GetReservationsDetailsAsync();
 }
--- RestaurantReservation/RestaurantReservation.Db/Repositories/ReservationRepository.cs
+++ RestaurantReservation/RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Interfaces;
 using RestaurantReservation.Db.Models.Entities;
+using RestaurantReservation.Db.Models.Views;
 
 namespace RestaurantReservation.Db.Repositories;
 
@@ -12,5 +13,10 @@ public class ReservationRepository(ApplicationDbContext context)
         return await context.Reservations
             .Where(r => r.CustomerId == customerId)
             .ToListAsync();
+    }
+
+    public async Task<IEnumerable<ReservationDetails>> GetReservationsDetailsAsync()
+    {
+        return await context.ReservationsDetails.ToListAsync();
     }
 }
