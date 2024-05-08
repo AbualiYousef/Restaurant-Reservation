d0091f73fc9bd0bc3d630bc2f659f58ad136700f
Yousef Abuali <abualiyousef@outlook.com>
2024-05-08 23:35:16 +0300

    Change totalAmount attribute data type in order model

---
+++ RestaurantReservation/RestaurantReservation.Db/Migrations/20240508203424_ChangeTotalAmountAttributeDataTypeToDecimal.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTotalAmountAttributeDataTypeToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 7, 23, 34, 20, 424, DateTimeKind.Local).AddTicks(9505), 45m });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 6, 23, 34, 20, 424, DateTimeKind.Local).AddTicks(9596), 30m });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 5, 23, 34, 20, 424, DateTimeKind.Local).AddTicks(9605), 60m });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 4, 23, 34, 20, 424, DateTimeKind.Local).AddTicks(9612), 22m });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 3, 23, 34, 20, 424, DateTimeKind.Local).AddTicks(9618), 80m });
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 11, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 12, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalAmount",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 6, 9, 46, 5, 601, DateTimeKind.Local).AddTicks(4094), 45 });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 5, 9, 46, 5, 601, DateTimeKind.Local).AddTicks(4166), 30 });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 4, 9, 46, 5, 601, DateTimeKind.Local).AddTicks(4173), 60 });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 3, 9, 46, 5, 601, DateTimeKind.Local).AddTicks(4179), 22 });
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "OrderDate", "TotalAmount" },
+                values: new object[] { new DateTime(2024, 5, 2, 9, 46, 5, 601, DateTimeKind.Local).AddTicks(4185), 80 });
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 10, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 11, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 12, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
--- RestaurantReservation/RestaurantReservation.Db/Models/Entities/Order.cs
+++ RestaurantReservation/RestaurantReservation.Db/Models/Entities/Order.cs
@@ -4,7 +4,7 @@ public class Order
 {
     public int Id { get; set; }
     public DateTime OrderDate { get; set; }
-    public int TotalAmount { get; set; }
+    public decimal TotalAmount { get; set; }
     public int? EmployeeId { get; set; }
     public int? ReservationId { get; set; }
     public Employee Employee { get; set; }
