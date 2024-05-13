5b917dd8ad457380082510124cd4ab87daf95f2c
Yousef Abuali <abualiyousef@outlook.com>
2024-05-13 08:06:00 +0300

    Change onDelete behavior for tables configurations

--- RestaurantReservation/RestaurantReservation.Db/Migrations/20240513050521_ChangeTablesConfigurations.Designer.cs
+++ RestaurantReservation/RestaurantReservation.Db/Migrations/20240513050521_ChangeTablesConfigurations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantReservation.Db;
 
@@ -11,9 +12,11 @@ using RestaurantReservation.Db;
 namespace RestaurantReservation.Db.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240513050521_ChangeTablesConfigurations")]
+    partial class ChangeTablesConfigurations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestaurantReservation/RestaurantReservation.Db/Migrations/20240513050521_ChangeTablesConfigurations.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTablesConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Restaurants_RestaurantId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantId",
+                table: "MenuItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Reservations_ReservationId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Reservations",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReservationId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MenuItemId",
+                table: "OrderItems",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RestaurantId",
+                table: "MenuItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 12, 8, 5, 20, 408, DateTimeKind.Local).AddTicks(3286));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 11, 8, 5, 20, 408, DateTimeKind.Local).AddTicks(3339));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 10, 8, 5, 20, 408, DateTimeKind.Local).AddTicks(3344));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 9, 8, 5, 20, 408, DateTimeKind.Local).AddTicks(3348));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 8, 8, 5, 20, 408, DateTimeKind.Local).AddTicks(3351));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 16, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 17, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 18, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 19, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 20, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Restaurants_RestaurantId",
+                table: "Employees",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantId",
+                table: "MenuItems",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId",
+                principalTable: "MenuItems",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Reservations_ReservationId",
+                table: "Orders",
+                column: "ReservationId",
+                principalTable: "Reservations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Restaurants_RestaurantId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantId",
+                table: "MenuItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Reservations_ReservationId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ReservationId",
+                table: "Orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MenuItemId",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RestaurantId",
+                table: "MenuItems",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 9, 18, 55, 6, 888, DateTimeKind.Local).AddTicks(6776));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 8, 18, 55, 6, 888, DateTimeKind.Local).AddTicks(6832));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 7, 18, 55, 6, 888, DateTimeKind.Local).AddTicks(6836));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 6, 18, 55, 6, 888, DateTimeKind.Local).AddTicks(6840));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2024, 5, 5, 18, 55, 6, 888, DateTimeKind.Local).AddTicks(6843));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 13, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 14, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 15, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 16, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2024, 5, 17, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Restaurants_RestaurantId",
+                table: "Employees",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_Restaurants_RestaurantId",
+                table: "MenuItems",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_MenuItems_MenuItemId",
+                table: "OrderItems",
+                column: "MenuItemId",
+                principalTable: "MenuItems",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Reservations_ReservationId",
+                table: "Orders",
+                column: "ReservationId",
+                principalTable: "Reservations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Customers_CustomerId",
+                table: "Reservations",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
